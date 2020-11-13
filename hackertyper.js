beacdface1dac8e69a8ec6b19c28c6327bb06147
BusterTornado <53686340+hreynolds1@users.noreply.github.com>
2020-11-13 10:59:51 +1000

    hackerman

--- hackertyper.js
+++ hackertyper.js
@@ -9,7 +9,26 @@ document.getElementById("inputfile").addEventListener("change", function() {
     fr.onload=function(){
         resulttext=fr.result.split('\n')
         for (i in resulttext){
-            resulttext[i]=resulttext[i].split('')
+            splitlist=[]
+            for (x in resulttext[i]){
+                switch(resulttext[i][x].charCodeAt(0)){
+                    case 9:
+                        for (n=0;n<4;n++){splitlist.push("\u00A0")}
+                        break;
+                    case 32:
+                        splitlist.push("\u00A0")
+                        break;
+                    default:
+                        splitlist.push(resulttext[i][x])
+                        break;
+                }
+                /*console.log(resulttext[i][x].charCodeAt(0))
+                console.log(resulttext[i][x])*/
+            }
+            console.log(splitlist)
+            resulttext[i]=splitlist.join("")
+            console.log(resulttext[i])
+            console.log(resulttext[i].length)
         }
     }
     filecontent=fr.readAsText(this.files[0])
@@ -17,7 +36,8 @@ document.getElementById("inputfile").addEventListener("change", function() {
 })
 
 document.onkeypress=function(){
-    hackertext.innerHTML+=resulttext[currentline].slice(currentchar,currentchar+speed).join("")
+    console.log(resulttext[currentline].substring(currentchar,currentchar+speed).split(""))
+    hackertext.innerHTML+=resulttext[currentline].substring(currentchar,currentchar+speed)
     currentchar+=speed
     if (currentchar>resulttext[currentline].length){
         if (currentline+1==resulttext.length){currentline=0}else{currentline+=1}
